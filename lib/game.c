fe482948ca041d160e6a3d943d0ac69dbaff645e
minishrink <>
2019-12-24 17:20:11 +0000

    Rejig move logic
    Signed-off-by: minishrink <>

--- lib/game.c
+++ lib/game.c
@@ -2,30 +2,31 @@
 
 #include "game.h"
 #include "helpers.h"
-#include <stdlib.h>
 
 /**** DEFINES ***/
 
 #define MOVELEN   (2u)
+#define NUMOFFSET (49u)
 #define HDRSTART  (65u)
 #define HDREND    (HDRSTART + BOARDLEN)
-#define NUMOFFSET (49u)
 
 /***** PRIVATE IMPLEMENTATION *****/
 
 /* FIXME This function is not generalisable at all */
 Move* parseMove(CHAR m[MOVELEN], SymbolT Piece)  {
+  CHAR col = m[0];
+  CHAR row = m[1];
+  // DEBUGF("Move: %s -> (Col: %d, Row: %d)\n", m, row, col);
   MALLOC(Move, pMove);
-  CHAR col, row;
   pMove->Piece = Piece;
-  if (HDRSTART <= (CHAR) m[0] && (CHAR) m[0] < HDREND) {
+  if (BETWEEN(HDRSTART, col, HDREND))  {
     // convert letter to column index
-    col = m[0] - HDRSTART;
+    col       -= (CHAR) HDRSTART;
     pMove->Col = col;
   }
   // convert CHAR to row index
-  row = m[1] - NUMOFFSET;
-  if (row < BOARDLEN) {
+  if (BETWEEN(0u, row, BOARDLEN + NUMOFFSET)) {
+    row       -= (CHAR) NUMOFFSET;
     pMove->Row = row;
   }
   DEBUGF("Move: %s -> (Row: %d, Col: %d)\n", m, pMove->Row, pMove->Col);
